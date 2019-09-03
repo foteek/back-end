5b3a9a178e41364c50891ae5bb9549e365fd2eb9
Rod <rod.bromberg@yahoo.com>
2019-09-02 23:25:07 -0400

    added cors for fe access

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const graphqlHttp = require("express-graphql");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const cors = require("cors");
 const { buildSchema } = require("graphql");
-const User = require('./models/user')
-const port = process.env.PORT || 3000
+const User = require("./models/user");
+const port = process.env.PORT || 3000;
 
 const server = express();
+server.use(cors());
+server.use(express.json());
 
-server.use(express.json())
-
-server.use("/users", graphqlHttp({
-  schema: buildSchema(`
+server.use(
+  "/users",
+  graphqlHttp({
+    schema: buildSchema(`
     type User {
       _id: ID!
       firstname: String!
@@ -38,36 +41,45 @@ server.use("/users", graphqlHttp({
     `),
     rootValue: {
       users: () => {
-
-        return User.find().then(users => {
-          return users
-        }).catch(err => {
-          throw err
-        })
+        return User.find()
+          .then(users => {
+            return users;
+          })
+          .catch(err => {
+            throw err;
+          });
       },
-      createUser: (args) => {
+      createUser: args => {
         const user = new User({
           firstname: args.userInput.firstname,
           lastname: args.userInput.lastname
-        })
+        });
 
-        return user.save().then(res => {
-          return res
-        }).catch(err => {
-          throw err
-        })
+        return user
+          .save()
+          .then(res => {
+            return res;
+          })
+          .catch(err => {
+            throw err;
+          });
       }
     },
     graphiql: true
-}))
+  })
+);
 
-
-mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@foteekcluster-nv3fj.mongodb.net/test?retryWrites=true&w=majority`, {
-  useNewUrlParser: true }
-)
-.then(() => {
-    server.listen(port)
-})
-.catch(err => {
-  throw err
-})
+mongoose
+  .connect(
+    process.env.MONGODB_URI ||
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@foteekcluster-nv3fj.mongodb.net/test?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => {
+    server.listen(port);
+  })
+  .catch(err => {
+    throw err;
+  });
