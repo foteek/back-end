37e5b9a7cfeae3d2fb18a9dc393e5ecca3a12638
Christopher Atoki <christopher.atoki@yahoo.com>
2019-09-10 20:56:06 -0400

    Revert "bringing in backend/token"

---
+++ index.js
@@ -0,0 +1,86 @@
+require("dotenv").config();
+const express = require("express");
+const graphqlHttp = require("express-graphql");
+const { graphqlExpress } = require('apollo-server-express')
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { buildSchema } = require("graphql");
+const User = require("./models/user");
+const port = process.env.PORT || 3000;
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.use(
+  "/users",
+  graphqlHttp({
+    schema: buildSchema(`
+    type User {
+      _id: ID!
+      firstname: String!
+      lastname: String!
+    }
+
+    input UserInput {
+      firstname: String!
+      lastname: String!
+    }
+
+    type userQuery {
+      users: [User!]!
+    }
+
+    type userMutation {
+      createUser(userInput: UserInput): User
+    }
+
+    schema {
+      query: userQuery
+      mutation: userMutation
+    }
+    `),
+    rootValue: {
+      users: () => {
+        return User.find()
+          .then(users => {
+            return users;
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createUser: args => {
+        const user = new User({
+          firstname: args.userInput.firstname,
+          lastname: args.userInput.lastname
+        });
+
+        return user
+          .save()
+          .then(res => {
+            return res;
+          })
+          .catch(err => {
+            throw err;
+          });
+      }
+    },
+    graphiql: true
+  })
+);
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI ||
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@foteekcluster-nv3fj.mongodb.net/test?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => {
+    server.listen(port);
+  })
+  .catch(err => {
+    throw err;
+  });
--- models/user.js
+++ models/user.js
@@ -1,44 +1,16 @@
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
-const bcrypt = require("bcrypt");
-
-// const passportLocalMongoose = require("passport-local-mongoose");
-
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-
-  password: {
+const userSchema = new Schema({
+  firstname: {
     type: String,
     required: true
   },
-
-  email: {
+  lastname: {
     type: String,
-    require: true
+    required: true
   }
-});
+})
 
-// can be used to implement local passport
-// UserSchema.plugin(passportLocalMongoose);
-
-UserSchema.pre("save", function(next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) return next;
-    bcrypt.hash(this.password, salt, (err, hash) => {
-      if (err) return next(err);
-      this.password = hash;
-      next();
-    });
-  });
-});
-
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema)
